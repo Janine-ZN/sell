eb50d5ec1d54195630cb67ec05056467aa1a3bbd
link12st <3230817960@qq.com>
2018-12-10 20:40:17 +0800

    路由配置

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,33 @@
   <div id="app">
     <v-header></v-header>
     <div class="tab">
-      <div class="tab-item">商品</div>
-      <div class="tab-item">评论</div>
-      <div class="tab-item">商家</div>
+      <div class="tab-item">
+        <!-- 使用指令 router-link 组件来导航 -->
+        <!-- 通过传入  to 属性指定链接 -->
+        <!-- <router-link> 默认会被渲染成一个 <a> 标签 -->
+        <router-link to="/goods">商品
+        </router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/ratings">评论
+        </router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/seller">商家
+        </router-link>
+      </div>
     </div>
-    <div class="footer">
+    <!-- 路由外链 : 点击上面的 v-link 修改下面的router-view -->
+    <router-view></router-view>
+    <!--使用 router-view 替换 content-->
+    <!--<div class="content">
       I am footer
-    </div>
+    </div>-->
   </div>
 </template>
 
 <script>
-  import header from './components/header/Header.vue'
+  import header from './components/header/header.vue'
   export default{
       components: {
           'v-header': header
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+// 引入goods模板
+import goods from '../components/goods/goods'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: []
+  routes: [
+    {path: '/goods', component: goods}
+  ]
 })
