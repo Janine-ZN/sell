e9a38a5140e2c70d8fd0c8d8c9cda924d6773a12
link12st <3230817960@qq.com>
2018-12-12 10:06:39 +0800

    新添两个路由

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,15 @@ import Vue from 'vue'
 import Router from 'vue-router'
 // 引入goods模板
 import goods from '../components/goods/goods'
+import ratings from '../components/ratings/ratings'
+import seller from '../components/seller/seller'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {path: '/goods', component: goods}
+    {path: '/goods', component: goods},
+    {path: '/ratings', component: ratings},
+    {path: '/seller', component: seller}
   ]
 })
